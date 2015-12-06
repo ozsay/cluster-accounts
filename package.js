2c6f0212971006e4b1b3fefebaa932db0408b486
Oz <ozsayag@gmail.com>
2015-12-06 17:59:38 +0200

    initial commit

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+    name: "ozsay:cluster-accounts",
+    summary: "A solution for accounts/authentication in meteorhacks:cluster",
+    version: "0.1.0",
+    git: "https://github.com/ozsay/cluster-accounts.git",
+    documentation: "README.md"
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('METEOR@0.9.2');
+
+    api.use('meteorhacks:cluster', ['server', 'client']);
+
+    api.imply('meteorhacks:cluster');
+
+    api.use('insecure', ['server', 'client'], {weak: true});
+
+    api.addFiles([
+        'lib/server/accountsCommon.js',
+        'lib/server/accountsConsumer.js',
+        'lib/server/accountsProvider.js'
+    ], ['server']);
+});
